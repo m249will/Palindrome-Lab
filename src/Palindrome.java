b5908c18a8e0ee7ea09f4b8c20e39e57a989340f
Will Schiesswohl <william.schiesswohl@vtsd.com>
2015-10-24 23:22:46 -0400

    Update Palindrome.java

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -4,73 +4,88 @@
 
 import java.util.Scanner;
 
-public class Palindrome
+public class Pal
 {
-	public static void main (String[] args)
-	{
+    public static void main (String[] args)
+    {
       Scanner input = new Scanner(System.in);
-		boolean notFinished = false;
-		do
-		{
-			System.out.print("Enter a string  ===>>  ");
-			String str = input.nextLine();
-			System.out.println();
-			System.out.println("Entered String:     " + str);
-			System.out.println("Palindrome:         " + PAlindrome.isPal(str));
-			System.out.println("Almost Palindrome:  " + PAlindrome.almostPal(str));	  // used only for the 100 point version
-			System.out.print("Do you wish to repeat this program [Y/N]?  ===>>  ");
-			String repeat = input.nextLine();
-			notFinished = (repeat.equals("Y")) || (repeat.equals("y"));
-			System.out.println();
-		}
-		while (notFinished);
-	}
+        boolean notFinished = false;
+        do
+        {
+            System.out.print("Enter a string  ===>>  ");
+            String str = input.nextLine();
+            System.out.println();
+            System.out.println("Entered String:     " + str);
+            System.out.println("Palindrome:         " + PAlindrome.isPal(str));
+            System.out.println("Almost Palindrome:  " + PAlindrome.almostPal(str));   // used only for the 100 point version
+            System.out.print("Do you wish to repeat this program [Y/N]?  ===>>  ");
+            String repeat = input.nextLine();
+            notFinished = (repeat.equals("Y")) || (repeat.equals("y"));
+            System.out.println();
+        }
+        while (notFinished);
+    }
 }
 
 
 
 class PAlindrome
 {
-	/*
-	 * Precondition:  s is an arbitrary String.
-	 * Postcondition: The value of true is returned if s is a Palindrome, false otherwise.
+    /*
+     * Precondition:  s is an arbitrary String.
+     * Postcondition: The value of true is returned if s is a Palindrome, false otherwise.
     * Note:          >>>>> This method is required for both the 80 point and the 100 point versions  <<<<<
-	 */
-	public static boolean isPal(String s)
-	{
-      return true;      // This statement is provided to allow initial compiling.
-	}  
+     */
+    public static boolean isPal(String s)
+    {
+      String s2 = new String();
+      s = s.toLowerCase();
+      int length = s.length()-1;
+      for (int k = length; k >= 0; k--)
+      {
+          s2 += s.substring(k,k+1);
+      }
+      
+      if (s.equals(s2))
+      {
+          return true;
+      }
+      
+      else
+      {
+          return false;
+      }
+    }  
    
    /*
-	 * Precondition:  s is a String of one character.
-	 * Postcondition: The value of true is returned if s is a letter and false otherwise.
+     * Precondition:  s is a String of one character.
+     * Postcondition: The value of true is returned if s is a letter and false otherwise.
     * Note:          >>>>> This method is only completed for the 100 point version  <<<<<
-	 */
+     */
    private static boolean isLetter(String letter)
    {
       return true;      // This statement is provided to allow initial compiling.
    }
    
-	/*
-	 * Precondition:  s is an arbitrary String.
-	 * Postcondition: All non-letter characters are removed from s, and this "purged" String is returned.
+    /*
+     * Precondition:  s is an arbitrary String.
+     * Postcondition: All non-letter characters are removed from s, and this "purged" String is returned.
     * Note:          >>>>> This method is only completed for the 100 point version  <<<<<
-	 */
-	private static String purge(String s)
-	{
+     */
+    private static String purge(String s)
+    {
       return "";        // This statement is provided to allow initial compiling.
-	}
+    }
    
    /*
-	 * Precondition:  s is an arbitrary String.
-	 * Postcondition: After purging all non-letter characters from s,
-	 *                the value of true is returned if the resulting String is a Palindrome, false otherwise.
+     * Precondition:  s is an arbitrary String.
+     * Postcondition: After purging all non-letter characters from s,
+     *                the value of true is returned if the resulting String is a Palindrome, false otherwise.
     * Note:          >>>>> This method is only completed for the 100 point version  <<<<<
-	 */
-	public static boolean almostPal(String s)
-	{
+     */
+    public static boolean almostPal(String s)
+    {
       return true;      // This statement is provided to allow initial compiling.
-	}
+    }
 
 }
-
